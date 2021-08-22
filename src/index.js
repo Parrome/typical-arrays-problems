f1097431e37852ece5eb5efc86958a697af77b71
Vladislav Paromov <parro@yandex.ru>
2021-08-22 20:44:15 +0300

    feat: index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minMass = 0;
+  if(array == undefined) {
+    return 0;
+  } else if (array.length < 1) {
+    return 0;
+  } else {  
+    minMass = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (minMass > array[i]) {
+        minMass = array[i];
+      } 
+    }
+  }
+  return minMass;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxMass = 0;
+  if(array == undefined) {
+    return 0;
+  }
+  if (array.length < 1) {
+    return 0;
+  } else {  
+    for (let i = 0; i < array.length; i++) {
+      if (maxMass < array[i]) {
+        maxMass = array[i];
+      } 
+    }
+    
+  }
+  return maxMass;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avgMass = 0;
+  if (array == undefined || array.length < 1) {
+    return 0;
+  } else {  
+    for (let i = 0; i < array.length; i++) {
+      avgMass = avgMass + array[i];
+      
+    }
+  }
+  return avgMass/array.length;
 }
